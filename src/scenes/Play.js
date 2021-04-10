35717ba7b2a366828a5e8c4b20eec01ef6f12e9c
sonia <atresonia@gmail.com>
2021-04-09 17:04:31 -0700

    Added spaceships and collision detection

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,12 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', 'assets/rocket.png');
         this.load.image('spaceship', 'assets/spaceship.png');
         this.load.image('starfield', 'assets/starfield.png');
+        //load spritesheet
+        this.load.spritesheet('explosion', 'assets/explosion.png', {
+            frameWidth: 64,
+            frameHeight: 32,
+            startFrame: 9
+        });
     }
 
     create() {
@@ -18,7 +24,7 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 
             'starfield').setOrigin(0, 0);
         // green UI background
-        this.add.rectangle(0, borderUISize + borderUISize, game.config.width,
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width,
             borderUISize * 2, 0x00FF00).setOrigin(0, 0);
 
         //white borders
@@ -34,9 +40,68 @@ class Play extends Phaser.Scene {
         // add rocket(player 1)
         this.p1Rocket  = new Rocket(this, game.config.width / 2, game.config.height - 
             borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+
+        // add spaceship (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize * 6, 
+            borderUISize * 4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize * 3, 
+            borderUISize * 5 + borderPadding * 2, 'spaceship', 0, 20).setOrigin(0, 0);
+            this.ship03 = new Spaceship(this, game.config.width, 
+                borderUISize * 6 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
+
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
+        //animation config
+        this.anims.create({
+           key: 'explode',
+           frames: this.anims.generateFrameNumbers('explosion', {
+               start: 0,
+               end: 9,
+               first: 0
+           }),
+           frameRate: 30
+        });
+
     }
 
     update() {
         this.starfield.tilePositionX -= starSpeed;
+
+        //update rocket
+        this.p1Rocket.update();
+        //update spaceships (x3)
+        this.ship01.update();
+        this.ship02.update();
+        this.ship03.update();
+
+        //check collisions 
+        if (this.checkCollision(this.p1Rocket, this.ship03)) {
+            this.p1Rocket.reset();
+            this.ship03.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            this.p1Rocket.reset();
+            this.ship02.reset();
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            this.p1Rocket.reset();
+            this.ship01.reset();
+        }
+    }
+
+    checkCollision(rocket, ship) {
+        //simple AABB checking
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y) {
+                return true;
+        } else {
+            return false;
+        } 
     }
 }
