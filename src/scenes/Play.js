6066d033e2e563fbdad42ef6301223118e0d6372
sonia <atresonia@gmail.com>
2021-04-10 20:51:05 -0700

    Finished game

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -89,9 +89,10 @@ class Play extends Phaser.Scene {
 
         // 60-second play clock
         scoreConfig.fixedWidth = 0;
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 
+                'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
 
@@ -110,6 +111,10 @@ class Play extends Phaser.Scene {
             this.ship01.update();   //update spaceships(x3)
             this.ship02.update();
             this.ship03.update();
+        }
+
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)) {
+            this.scene.start("menuScene");
         }
 
         //check collisions 
