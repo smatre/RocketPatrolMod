6ad59ccf45434c997067671ed99da4b438fef11e
sonia <atresonia@gmail.com>
2021-04-10 20:33:08 -0700

    Added sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -83,18 +83,34 @@ class Play extends Phaser.Scene {
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, 
             borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
+        
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
 
     }
 
     update() {
+        //check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+
         this.starfield.tilePositionX -= starSpeed;
 
-        //update rocket
-        this.p1Rocket.update();
-        //update spaceships (x3)
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if (!this.gameOver) {
+            this.p1Rocket.update(); //update rocket sprite
+            this.ship01.update();   //update spaceships(x3)
+            this.ship02.update();
+            this.ship03.update();
+        }
 
         //check collisions 
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
@@ -109,6 +125,7 @@ class Play extends Phaser.Scene {
             this.p1Rocket.reset();
             this.shipExplode(this.ship01);
         }
+
     }
 
     checkCollision(rocket, ship) {
@@ -139,6 +156,8 @@ class Play extends Phaser.Scene {
         this.p1Score += ship.points;
         //replaces contents with new value
         this.scoreLeft.text = this.p1Score;
+        //add explosion sound
+        this.sound.play('sfx_explosion');
 
     }
 }
