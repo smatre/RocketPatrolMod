ceae6d1c81211118be3ea9e5a86fc2e297f246d1
sonia <atresonia@gmail.com>
2021-04-21 15:15:54 -0700

    Final touches

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 //catch flies, display timer(10), 
 //create new animated sprite(10): pink splat after fly dies, new title screen(10): created 
 //own background image artwork, used different fonts, and rearranged layout 
-//added time to timer after successful hit (10): add time depending on score of fly
+//added time to timer after successful hit (20): add time depending on score of fly
 //allow player to control frog after "fired" (5), 
 //created a winning screen with a princess and a heart(5?)
 //all artwork is created by me
